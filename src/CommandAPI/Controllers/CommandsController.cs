d231c71c4d0f89af0901cef98aa00be4570238be
Ridwan Kolawole <olaniyi.jibola152@gmail.com>
2021-01-06 15:09:15 +0100

    initial commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase 
+    // if we inherit from Controller and not ControllerBase, it will also provide additional support which we dont need
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"this", "is", "hard", "coded"};
+        }
+    }
+}
